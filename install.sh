e4f5f200caef95645c56f6e2e9d6f8065dc0294e
kaitsh <kaitsh@d-git.de>
2019-01-25 14:45:49 -0300

    spacemacs: add spacemacs config

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 BASEDIR=$(dirname "$0")
 
-ln -s $BASEDIR/dot.mkshrc $HOME/.mkshrc
+#ln -s $BASEDIR/dot.mkshrc $HOME/.mkshrc
 ln -s $BASEDIR/dot.zshrc $HOME/.zshrc
+ln -s $BASEDIR/dot.spacemacs $HOME/.spacemacs
