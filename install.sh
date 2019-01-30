658c84a50bd8e0476ee0fb99e8d65cb6e7039656
kaitsh <kaitsh@d-git.de>
2019-01-30 15:20:41 -0300

    fix directory symlink

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@ SCRIPT="$(realpath $0)"
 BASEDIR=$(dirname "$SCRIPT")
 
 # install fonts
-ln -s $BASEDIR/dot.fonts $HOME/.fonts
+[ -d "$HOME/.fonts" ] && unlink $HOME/.fonts; ln -s $BASEDIR/dot.fonts $HOME/.fonts
 fc-cache
 
 # make dotfiles available
 ln -s $BASEDIR/dot.mkshrc $HOME/.mkshrc
 ln -s $BASEDIR/dot.zshrc $HOME/.zshrc
 ln -s $BASEDIR/dot.spacemacs $HOME/.spacemacs
-ln -s $BASEDIR/dot.dwm-status $HOME/.dwm-status
+[ -d "$HOME/.dwm-status" ] && unlink $HOME/.dwm-status; ln -s $BASEDIR/dot.dwm-status $HOME/.dwm-status
 ln -s $BASEDIR/dot.xinitrc $HOME/.xinitrc
