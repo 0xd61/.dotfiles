707931598d5adbaf2b8f1e7a08d9c4ca94f9cb75
kaitsh <kaitsh@d-git.de>
2019-01-30 15:08:40 -0300

    add new dotfiles to install script

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@
 SCRIPT="$(realpath $0)"
 BASEDIR=$(dirname "$SCRIPT")
 
-#ln -s $BASEDIR/dot.mkshrc $HOME/.mkshrc
+# install fonts
+ln -s $BASEDIR/dot.fonts $HOME/.fonts
+fc-cache
+
+# make dotfiles available
+ln -s $BASEDIR/dot.mkshrc $HOME/.mkshrc
 ln -s $BASEDIR/dot.zshrc $HOME/.zshrc
 ln -s $BASEDIR/dot.spacemacs $HOME/.spacemacs
+ln -s $BASEDIR/dot.dwm-status $HOME/.dwm-status
+ln -s $BASEDIR/dot.xinitrc $HOME/.xinitrc
