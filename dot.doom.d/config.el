547fcd794c28ee87a9ad3f0ca37d16c9fa89eb93
kaitsh <kaitsh@d-git.de>
2019-04-29 18:01:08 -0400

    doom: change tabs to 2

--- dot.doom.d/config.el
+++ dot.doom.d/config.el
@@ -2,3 +2,6 @@
 
 ;; Place your private configuration here
 (projectile-add-known-project "/home/kaitsh/Code/src/gitlab.com/samuu.dev/piari")
+
+(setq-default evil-shift-width 2 ;; set tabs to 2
+    tab-width 2)
