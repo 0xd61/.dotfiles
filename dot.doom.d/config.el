ca08c64cccb83783288efc24b3a67636133925ee
Daniel Glinka <kaitsh@d-git.de>
2019-08-07 23:36:14 +0000

    doom: remove project from conf

--- dot.doom.d/config.el
+++ dot.doom.d/config.el
@@ -1,9 +1,5 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here
-(projectile-add-known-project "/home/kaitsh/Code/src/gitlab.com/samuu.dev/piari")
-
-
 (setq-default evil-shift-width 2 ;; set tabs to 2
       tab-width 2)
 
