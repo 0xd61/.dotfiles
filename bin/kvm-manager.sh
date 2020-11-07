36d626f4eb41426f3d6448e85dc7f2b2aab67509
Daniel Glinka <kaitsh@degit.co>
2020-11-07 21:55:55 +0000

    Fix usb redirect

--- bin/kvm-manager.sh
+++ bin/kvm-manager.sh
@@ -60,14 +60,26 @@ fi
 if [ ! -z ${SPICE_PORT} ]; then
 	SPICE="-spice port=${SPICE_PORT},disable-ticketing \
 -device virtio-serial -chardev spicevmc,id=vdagent,name=vdagent -device virtserialport,chardev=vdagent,name=com.redhat.spice.0 \
--device ich9-usb-ehci1,id=usb \
--device ich9-usb-uhci1,masterbus=usb.0,firstport=0,multifunction=on \
--device ich9-usb-uhci2,masterbus=usb.0,firstport=2 \
--device ich9-usb-uhci3,masterbus=usb.0,firstport=4 \
--chardev spicevmc,name=usbredir,id=usbredirchardev1 -device usb-redir,chardev=usbredirchardev1,id=usbredirdev1 \
--chardev spicevmc,name=usbredir,id=usbredirchardev2 -device usb-redir,chardev=usbredirchardev2,id=usbredirdev2 \
--chardev spicevmc,name=usbredir,id=usbredirchardev3 -device usb-redir,chardev=usbredirchardev3,id=usbredirdev3"
+-device nec-usb-xhci,id=usb \
+-chardev spicevmc,name=usbredir,id=usbredirchardev1 \
+-device usb-redir,chardev=usbredirchardev1,id=usbredirdev1 \
+-chardev spicevmc,name=usbredir,id=usbredirchardev2 \
+-device usb-redir,chardev=usbredirchardev2,id=usbredirdev2 \
+-chardev spicevmc,name=usbredir,id=usbredirchardev3 \
+-device usb-redir,chardev=usbredirchardev3,id=usbredirdev3"
 fi
+
+# USB 2 config
+#-device ich9-usb-ehci1,id=usb \
+#-device ich9-usb-uhci1,masterbus=usb.0,firstport=0,multifunction=on \
+#-device ich9-usb-uhci2,masterbus=usb.0,firstport=2 \
+#-device ich9-usb-uhci3,masterbus=usb.0,firstport=4 \
+#-chardev spicevmc,name=usbredir,id=usbredirchardev1 \
+#-device usb-redir,chardev=usbredirchardev1,id=usbredirdev1 \
+#-chardev spicevmc,name=usbredir,id=usbredirchardev2 \
+#-device usb-redir,chardev=usbredirchardev2,id=usbredirdev2 \
+#-chardev spicevmc,name=usbredir,id=usbredirchardev3 \
+#-device usb-redir,chardev=usbredirchardev3,id=usbredirdev3
 
 ### generated variables
 
