35a5fcf67ccdebafcda80c497c8f291c539db395
Daniel Glinka <dgl@degit.co>
2021-10-22 11:43:54 +0000

    Update gf2 build

--- dot.nixpkgs/gf2.nix
+++ dot.nixpkgs/gf2.nix
@@ -1,5 +1,11 @@
 with import <nixpkgs> {};
-
+let
+  vim_configured = pkgs.vim_configurable.customize {
+    name = "vim";
+    wrapGui = true;
+    vimrcConfig.customRC = "";
+  };
+in
 stdenv.mkDerivation rec {
   pname = "gf2";
   version = "e49f7895fa2b568631f220f7d20b52c3d0d5a99a";
@@ -10,27 +16,36 @@ stdenv.mkDerivation rec {
     sha256 = "0mhwr8f84mpcakqbavxsg575jli5mrff5y6q5bizcy6af69bpwwm";
   };
 
+  nativeBuildInputs = [ makeWrapper ];
   buildInputs = [ xorg.libX11 gdb ];
 
   patchPhase = ''
     echo #!/usr/bin/env bash > build.sh
     patchShebangs \
     build.sh
+
+    #sed -i 's/vimServerName = "GVIM"/vimServerName = "VIM"/' gf2.cpp
   '';
 
   buildPhase = ''
-    ./build.sh
+    extra_flags=-DUI_FREETYPE_SUBPIXEL ./build.sh
   '';
 
   installPhase = ''
     mkdir -p "$out/bin"
     cp gf2 "$out/bin/"
+    runHook postInstall
+  '';
+
+  postInstall = with lib; ''
+    wrapProgram $out/bin/gf2 --prefix PATH : ${makeBinPath [ gdb vim_configured ]}
   '';
 
   meta = with lib; {
     description = "gf – A GDB Frontend";
     homepage = "https://github.com/nakst/gf";
     license = licenses.mit;
-    maintainers = teams.sage.members;
+    platforms = platforms.linux;
   };
 }
+
