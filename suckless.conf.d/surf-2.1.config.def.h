a3e875f09723a4600ed6360d6861b8522c1217f6
Daniel Glinka <dgl@degit.co>
2021-10-22 16:05:40 +0000

    Update newwindow url

--- suckless.conf.d/surf-2.1.config.def.h
+++ suckless.conf.d/surf-2.1.config.def.h
@@ -191,7 +191,7 @@ static Key keys[] = {
 	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_t,      toggle,     { .i = StrictTLS } },
 	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_m,      toggle,     { .i = Style } },
 
-	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_Return, newwindow,  { 1 } },
+	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_Return, newwindow,  { .v = "https://duckduckgo.com" } },
 };
 
 static char *searchengine = "https://duckduckgo.com/?q=";
