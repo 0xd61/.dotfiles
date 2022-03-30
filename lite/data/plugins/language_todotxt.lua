ec0e1f88575b419e6b638cb252e8c177f9ffbb81
Daniel Glinka <dgl@degit.co>
2022-03-30 13:55:22 -0400

    Apply todo.txt language to time.txt

--- lite/data/plugins/language_todotxt.lua
+++ lite/data/plugins/language_todotxt.lua
@@ -1,15 +1,15 @@
 local syntax = require "core.syntax"
 
 syntax.add {
-  files = { "todo.*%.txt$" },
+  files = { "todo.*%.txt$", "time.*%.txt$" },
   patterns = {
-    { pattern = "x%s.-\n",          type = "comment"  },
-    { pattern = "(%d+%-%d+%-%d+)",  type = "number"   },
-    { pattern = "%([A-Z]%)%s",      type = "literal"  },
+    { pattern = "^x%s.-\n",          type = "comment"  },
+    { pattern = "(%d%d%d%d%-%d%d%-%d%d)",  type = "number"   },
+    { pattern = "%([A-Z]%)%s",         type = "literal"  },
     { pattern = "%s%+[%w]*",        type = "keyword2" },
     { pattern = "%s@[%w]*",         type = "keyword"  },
     { pattern = "%s#[%w]*",         type = "symbol"   },
-    { pattern = "[%w]*:[%w_%-]*",   type = "string"   },
+    { pattern = "[%w]*:[%w_%-]*",      type = "string"   },
   },
   symbols = {
   },
