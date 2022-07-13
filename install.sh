7a101757915d3d7a4b4298d5d4879eeff3ff1090
Daniel Glinka <dgl@degit.co>
2022-07-13 12:36:14 -0400

    Add alactitty config

--- install.sh
+++ install.sh
@@ -17,13 +17,14 @@ ln -s $BASEDIR/dot.vimrc $HOME/.vimrc
 #ln -s $BASEDIR/dot.ctags $HOME/.ctags
 #ln -s $BASEDIR/dot.nbrc $HOME/.nbrc
 #ln -s $BASEDIR/dot.gdbinit $HOME/.gdbinit
+ln -s $BASEDIR/dot.alacritty.yml $HOME/.alacritty.yml
 ln -s $BASEDIR/dot.gf2_config.ini $HOME/.config/gf2_config.ini
 [ -d "$HOME/.vim" ] && unlink $HOME/.vim; ln -s $BASEDIR/dot.vim $HOME/.vim
 [ -d "$HOME/.dwm-status" ] && unlink $HOME/.dwm-status; ln -s $BASEDIR/dot.dwm-status $HOME/.dwm-status
 [ -d "$HOME/.local/bin" ] && unlink $HOME/.local/bin; ln -s $BASEDIR/bin $HOME/.local/bin
 #[ -d "$HOME/.config/ranger" ] && unlink $HOME/.config/ranger; ln -s $BASEDIR/ranger $HOME/.config/ranger
-[ -d "$HOME/.config/foot" ] && unlink $HOME/.config/foot; ln -s $BASEDIR/foot $HOME/.config/foot
-[ -d "$HOME/.config/yambar" ] && unlink $HOME/.config/yambar; ln -s $BASEDIR/yambar $HOME/.config/yambar
+#[ -d "$HOME/.config/foot" ] && unlink $HOME/.config/foot; ln -s $BASEDIR/foot $HOME/.config/foot
+#[ -d "$HOME/.config/yambar" ] && unlink $HOME/.config/yambar; ln -s $BASEDIR/yambar $HOME/.config/yambar
 #[ -d "$HOME/.doom.d" ] && unlink $HOME/.doom.d; ln -s $BASEDIR/dot.doom.d $HOME/.doom.d
 [ -d "$HOME/.weechat" ] && unlink $HOME/.weechat; ln -s $BASEDIR/dot.weechat $HOME/.weechat
 [ -d "$HOME/.nixpkgs" ] && unlink $HOME/.nixpkgs; ln -s $BASEDIR/dot.nixpkgs $HOME/.nixpkgs
