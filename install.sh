ae704f523d038a5b40aa82bc10eb410367cdb566
Daniel Glinka <dgl@degit.co>
2024-11-18 17:26:46 -0300

    Update fonts

--- install.sh
+++ install.sh
@@ -25,14 +25,12 @@ ln -s $BASEDIR/dot.gf2_config.ini $HOME/.config/gf2_config.ini
 [ -d "$HOME/.vim" ] && unlink $HOME/.vim; ln -s $BASEDIR/dot.vim $HOME/.vim
 [ -d "$HOME/.config/nvim" ] && unlink $HOME/.config/nvim; ln -s $BASEDIR/dot.nvim $HOME/.config/nvim
 [ -d "$HOME/.config/focus-editor" ] && unlink $HOME/.config/focus-editor; ln -s $BASEDIR/dot.focus $HOME/.config/focus-editor
-[ -d "$HOME/.config/jasspa" ] && unlink $HOME/.config/jasspa; ln -s $BASEDIR/dot.me $HOME/.config/jasspa
 [ -d "$HOME/.dwm-status" ] && unlink $HOME/.dwm-status; ln -s $BASEDIR/dot.dwm-status $HOME/.dwm-status
 [ -d "$HOME/.local/bin" ] && unlink $HOME/.local/bin; ln -s $BASEDIR/bin $HOME/.local/bin
 #[ -d "$HOME/.config/ranger" ] && unlink $HOME/.config/ranger; ln -s $BASEDIR/ranger $HOME/.config/ranger
 #[ -d "$HOME/.config/foot" ] && unlink $HOME/.config/foot; ln -s $BASEDIR/foot $HOME/.config/foot
 #[ -d "$HOME/.config/yambar" ] && unlink $HOME/.config/yambar; ln -s $BASEDIR/yambar $HOME/.config/yambar
 #[ -d "$HOME/.doom.d" ] && unlink $HOME/.doom.d; ln -s $BASEDIR/dot.doom.d $HOME/.doom.d
-[ -d "$HOME/.weechat" ] && unlink $HOME/.weechat; ln -s $BASEDIR/dot.weechat $HOME/.weechat
 [ -d "$HOME/.nixpkgs" ] && unlink $HOME/.nixpkgs; ln -s $BASEDIR/dot.nixpkgs $HOME/.nixpkgs
 ln -s $BASEDIR/dot.xprofile $HOME/.xprofile
 ln -s $BASEDIR/dot.xinitrc $HOME/.xinitrc
