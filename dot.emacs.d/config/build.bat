db0df64a79f02fe336f7bf23d7c3c234c95904bd
Daniel Glinka <dgl@degit.co>
2024-11-18 22:30:24 -0300

    Start creating org emacs config

---
+++ dot.emacs.d/config/build.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Emacs\emacs-29.4\bin\emacs.exe" -q --batch --eval "(require 'ob-tangle)" --eval "(setq org-confirm-babel-evaluate nil)" --eval "(org-babel-tangle-file \"%~1\")"
---
+++ dot.emacs.d/config/early-init.el
@@ -0,0 +1,24 @@
+(setq package-enable-at-startup nil
+      inhibit-startup-message   t
+      frame-resize-pixelwise    t  ; fine resize
+      package-native-compile    t) ; native compile packages
+(scroll-bar-mode -1)               ; disable scrollbar
+(tool-bar-mode -1)                 ; disable toolbar
+(tooltip-mode -1)                  ; disable tooltips
+(set-fringe-mode 10)               ; give some breathing room
+(menu-bar-mode -1)                 ; disable menubar
+(blink-cursor-mode 0)              ; disable blinking cursor
+
+(setq frame-inhibit-implied-resize t)
+(setq inhibit-compacting-font-c
+(setq gc-cons-threshold most-positive-fixnum)
+
+(setq undo-limit 20000000)
+(setq undo-strong-limit 40000000)
+
+(defvar file-name-handler-alist-original file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+(setq site-run-file nil)
+
+(setq gc-cons-threshold 100000000)
