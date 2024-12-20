bb3ef4acd2b27f7e5a2eb2190a9994c6e8a9cb2e
Daniel Glinka <dgl@degit.co>
2024-12-19 21:09:48 -0300

    Update wezterm

--- dot.wezterm.lua
+++ dot.wezterm.lua
@@ -7,33 +7,17 @@ local config = wezterm.config_builder()
 
 config.font = wezterm.font('Input Mono', {weight = 'Regular'})
 
-config.colors = {
-  -- The default text color
-  foreground = 'silver',
-  -- The default background color
-  background = '#013137',
+config.color_scheme = 'neobones_dark'
 
-  -- -- Overrides the cell background color when the current cell is occupied by the
-  -- -- cursor and the cursor style is set to Block
-  -- cursor_bg = '#52ad70',
-  -- -- Overrides the text color when the current cell is occupied by the cursor
-  -- cursor_fg = 'black',
-  -- -- Specifies the border color of the cursor when the cursor style is set to Block,
-  -- -- or the color of the vertical or horizontal bar when the cursor style is set to
-  -- -- Bar or Underline.
-  -- cursor_border = '#52ad70',
-
-  -- -- the foreground color of selected text
-  -- selection_fg = 'black',
-  -- -- the background color of selected text
-  -- selection_bg = '#fffacd',
-
-  -- -- The color of the scrollbar "thumb"; the portion that represents the current viewport
-  -- scrollbar_thumb = '#222222',
-
-  -- -- The color of the split lines between panes
-  -- split = '#444444',
+-- start a connect session by default
+-- to be able to save the session state.
+config.unix_domains = {
+  {
+    name = 'unix',
+  },
 }
 
--- and finally, return the configuration to wezterm
+config.default_gui_startup_args = { 'connect', 'unix' }
+
+
 return config
