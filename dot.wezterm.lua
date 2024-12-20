afc594ea03cbbb9c4d374485869c1c99d0e2494c
Daniel Glinka <dgl@degit.co>
2024-12-20 17:42:30 -0300

    Update files

--- dot.wezterm.lua
+++ dot.wezterm.lua
@@ -19,5 +19,64 @@ config.unix_domains = {
 
 config.default_gui_startup_args = { 'connect', 'unix' }
 
+wezterm.on("gui-startup", function()
+  local tab, pane, window = mux.spawn_window{}
+  window:gui_window():maximize()
+end)
+
+-- Keybinds
+config.leader = { key = '\\', mods = 'ALT', timeout_milliseconds = 1000 }
+
+-- Table mapping keypresses to actions
+config.keys = {
+  -- Sends ESC + b and ESC + f sequence, which is used
+  -- for telling your shell to jump back/forward.
+  {
+    -- When the left arrow is pressed
+    key = 'LeftArrow',
+    -- With the "Option" key modifier held down
+    mods = 'ALT',
+    -- Perform this action, in this case - sending ESC + B
+    -- to the terminal
+    action = wezterm.action.SendString '\x1bb',
+  },
+  {
+    key = 'RightArrow',
+    mods = 'ALT',
+    action = wezterm.action.SendString '\x1bf',
+  },
+
+  {
+    key = ',',
+    mods = 'SUPER',
+    action = wezterm.action.SpawnCommandInNewTab {
+      cwd = wezterm.home_dir,
+      args = { 'nvim', wezterm.config_file },
+    },
+  },
+
+  {
+    -- I'm used to tmux bindings, so am using the quotes (") key to
+    -- split horizontally, and the percent (%) key to split vertically.
+    key = '5',
+    -- Note that instead of a key modifier mapped to a key on your keyboard
+    -- like CTRL or ALT, we can use the LEADER modifier instead.
+    -- This means that this binding will be invoked when you press the leader
+    -- (CTRL + A), quickly followed by quotes (").
+    mods = 'LEADER',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = '-',
+    mods = 'LEADER',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'f',
+    mods = 'LEADER',
+    -- Present a list of existing workspaces
+    action = wezterm.action.ShowLauncherArgs { flags = 'FUZZY|WORKSPACES' },
+  },
+}
 
 return config
