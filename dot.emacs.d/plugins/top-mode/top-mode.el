458716aac6e8f4060e08d27236da3c5180a24e17
Daniel Glinka <dgl@degit.co>
2025-06-05 22:27:11 -0300

    Update keybinds

--- dot.emacs.d/plugins/top-mode/top-mode.el
+++ dot.emacs.d/plugins/top-mode/top-mode.el
@@ -2,6 +2,52 @@
 
 (require 'cl-lib)
 
+;; Useful functions from other packages:
+;; - xah fly keys (https://github.com/xahlee/xah-fly-keys/blob/master/xah-fly-keys.el)
+(defun xah-beginning-of-line-or-block ()
+  "Move cursor to beginning of indent or line, end of previous block, in that order.
+
+If `visual-line-mode' is on, beginning of line means visual line.
+
+URL `http://xahlee.info/emacs/emacs/emacs_move_by_paragraph.html'
+Created: 2018-06-04
+Version: 2024-10-30"
+  (interactive)
+  (let ((xp (point)))
+    (if (or (eq (point) (line-beginning-position))
+            (eq last-command this-command))
+        (when (re-search-backward "\n[\t\n ]*\n+" nil 1)
+          (skip-chars-backward "\n\t ")
+          ;; (forward-char)
+          )
+      (if visual-line-mode
+          (beginning-of-visual-line)
+        (if (eq major-mode 'eshell-mode)
+            (beginning-of-line)
+          (back-to-indentation)
+          (when (eq xp (point))
+            (beginning-of-line)))))))
+
+(defun xah-end-of-line-or-block ()
+  "Move cursor to end of line or next block.
+
+- When called first time, move cursor to end of line.
+- When called again, move cursor forward by jumping over any sequence of whitespaces containing 2 blank lines.
+- if `visual-line-mode' is on, end of line means visual line.
+
+URL `http://xahlee.info/emacs/emacs/emacs_move_by_paragraph.html'
+Created: 2018-06-04
+Version: 2024-10-30"
+  (interactive)
+  (if (or (eq (point) (line-end-position))
+          (eq last-command this-command))
+      (re-search-forward "\n[\t\n ]*\n+" nil 1)
+    (if visual-line-mode
+        (end-of-visual-line)
+      (end-of-line))))
+
+;; TOP MODE CODE STARTS HERE
+
 (defvar top-mode-map (make-sparse-keymap)
   "Keymap for `top-mode`.")
 
